5eb7ccdf1cc238f66f840b5c6e8987a5743a9695
Kanaja Hunt <122937747+KanajaHunt@users.noreply.github.com>
2023-07-15 18:31:50 -0400

    multiply array

---
+++ reduce-grow.js
@@ -0,0 +1,6 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// My answer
+const grow = (nums) => nums.reduce((product, num) => product * num, 1);
