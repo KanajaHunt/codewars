b40ca09a04975ee32acbff6eaead0a3e31fbf41e
Kanaja Hunt <KanajaHunt@gmail.com>
2023-07-21 16:10:51 -0400

    added new codewars

---
+++ 8kyu/square-argument.js
@@ -0,0 +1,14 @@
+// Function 2 - squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// My answer
+// PREP
+// return, ^2
+
+function square(n){
+  return(n**2)
+}
+
+// output: returnSquare()
+
